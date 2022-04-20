942b953821f0299e81b71fdc96cd24aba1442327
Jan <>
2022-04-20 16:15:36 +0200

    Initial Setup

---
+++ src/main/java/com/janurb/awesomeinterviewmanager/AwesomeinterviewmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.janurb.awesomeinterviewmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeinterviewmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeinterviewmanagerApplication.class, args);
+	}
+
+}
